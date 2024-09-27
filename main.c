3f79260ac909889e3e65e0df505822856a1259de
Gotta003 <94313973+Gotta003@users.noreply.github.com>
2024-09-27 12:42:32 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,43 @@
 #include "msp.h"
+#include "boolean.h"
+#define COLORS 6
+#define TENTATIVES 10
+#define DIM 4
+#define DOUBLES false;
 
+typedef enum {START} State;
+typedef struct {
+	State state;
+	void (*state_function)(void);
+} StateMachine;
+typedef struct {
+	int dim;
+	char difficulty;
+	bool doubles;
+	int tentatives;
+	char* seq_to_guess;
+} Game;
+typedef struct {
+	int count;
+	int dim;
+	int* seq_user;
+} Tentative;
 
+Game game;
+Tentative tentative;
+char* chronology;
+/* FUNCTION DECLARATIONS */
+void fn_INIT();
+/* FINITE STATE MACHINE IMPLEMENTATION */
+StateMachine fms[]={
+	{INIT, fn_INIT} 
+};
+
+State current_state=INIT;
+
+void fn_INIT() {
+
+}
 /**
  * main.c
  */
