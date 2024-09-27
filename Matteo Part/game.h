77ce8888bea6a15c92f3b92bdccb8db7a19d18e0
Gotta003 <94313973+Gotta003@users.noreply.github.com>
2024-09-27 18:55:21 +0200

    Update game.h

--- Matteo Part/game.h
+++ Matteo Part/game.h
@@ -5,37 +5,8 @@
 #include "stdlib.h"
 #include "time.h"
 #include "stdbool.h"
+#include "fsm.h"
 
-/* Constants */
-#define COLORS 6                // Number of colors available
-#define DIFFICULTY 'A'         // Default difficulty level
-#define TENTATIVES 10          // Number of attempts allowed
-#define DIM 4                   // Dimension of the sequence
-#define DOUBLES false           // Whether doubles are allowed
-
-/* ENUM Declaration */
-typedef enum {START, KEY_WOUT_DOUB, KEY_WH_DOUB, RESET_TENT, ERROR} State;
-
-/* Struct declarations */
-typedef struct {
-    int dim;                   // Dimension of the sequence
-    char difficulty;           // Difficulty level of the game
-    bool doubles;              // Flag to indicate if doubles are allowed
-    int count_tent;           // Count of current attempts
-    int tentatives;            // Total number of allowed attempts
-    char* seq_to_guess;        // Sequence to be guessed
-    char* chronology;          // History of attempts
-} Game;
-
-typedef struct {
-    int count;                // Count of user attempts
-    int dim;                  // Dimension of user's sequence
-    char* seq_user;           // User's guessed sequence
-} Tentative;
-/* Global variables */
-extern Game game;                  // Game struct instance
-extern Tentative tentative;        // Tentative struct instance
-extern State current_state;  // Initialize current state
 /* Function declarations */
 void fn_START(void);                          // Function for the START state
 void fn_KEY_WOUT_DOUB(void);                  // Function for the KEY_WOUT_DOUB state
