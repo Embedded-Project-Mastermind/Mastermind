06046a7f1951344770b4ff43be27dcd786d2ad0a
ale-bena <127418373+ale-bena@users.noreply.github.com>
2024-10-02 13:35:38 +0200

    Add files via upload

---
+++ src/gamelogic2.c
@@ -0,0 +1,125 @@
+#include "stdio.h"
+#include "stdlib.h"
+#include "stdbool.h"
+#include "fsm.h"
+#include "gamelogic1.h"
+#include "gamelogic2.h"
+
+bool is_empty(void){     //tentative.count=null, then the array is empty (true, if not empty false) 
+     (tentative.count = 0) ? true : false; 	
+}
+
+bool is_full(void){   	//If count=dim, then array is full (true, if != then false)
+(tentative.count  = tentative.dim) ? true : false;
+}
+
+void text_selection(bool sel){     //selects which text to display
+	if(sel){             //if true -> button menu
+	printf("Selection menu: \n- Type 1 to insert an input \n- Type 2 to press confirm\n- Type 3 to press back \n");
+	}
+	else{             //if false -> color menu
+		printf("Colors selection menu:\n- 1 red \n- 2 green \n- 3 blue \n-4 yellow \n- 5 purple \n- 6 cyan \n ");
+	}
+}
+
+void full_text(){
+     printf("The array is full, click 2 to confirm and 3 to go back");    
+}
+
+int read(int dim){     //reads the input from keyboard and verifies if it's correct
+    int value = 0;
+    do{       //done with a do while so we are sure that it executes
+	   text_selection(selection);  //da mettere all'esterno se non vogliamo che ristampi il testo ogni volta(poco rilevante)
+       value = scanf("%d", value);       //scans the value to analize
+       if(value> 0 && value <= dim){     //value must respect these constraints
+	             return value;
+       }
+       }while(value < 1 || value > dim);	//do while exit condition
+}
+
+void handle_wait(int val){
+     if(current_state == WAIT){ text_selection(selection);}
+     if(current_state == WAIT_FULL) { full_text(); }
+	switch(val){
+		case 1: if(current_state == WAIT){  
+					current_state  = ELABORATE;}
+				else{
+					printf("Can't continue!");   //error message
+				}
+				break;
+		case 2:	if(current_state == WAIT_FULL){
+					current_state = ELABORATE_TENT;
+				}
+				else {
+					printf("Can't elaborate because the array is not full!");  //error message
+				}
+				break;
+		case 3: if(!is_empty()){
+					current_state = ELIMINATE_COLOR;
+				}
+				else {
+					printf("Can't eliminate color, because the array is empty");        //error message
+				}
+		default: exit(1);
+	}
+}
+
+void wait() { 
+     selection = true;  //to select menu text from text_selection()
+     value = read(OPTIONS);   //to read the value in input
+     selection = false;   //to change text selection in function text_selection
+     handle_wait(value); //
+	
+}
+
+void wait_full() {  //wait function when the array is full
+    full_text();   //prints the text that tells what instructions you can do
+	int value = read(OPTIONS);   //gets the value from the keyboard
+	handle_wait(value);
+}
+
+void insert_color(char color){
+	if(!(is_full())){
+        tentative.seq_user[tentative.count]=color; 
+        tentative.count +=1;
+    }
+}
+
+
+void eliminate_color( ){  //eliminates color in position tentative.count from seq_user array
+     if(!(is_empty())){
+     tentative.count -= 1;   //decrements tentative.count
+     tentative.seq_user[tentative.count]= '\0';   //eliminate color 
+	}
+	
+}
+
+bool elaborate(){  
+     selection = false;   //set boolean variable selection to false to display the correct text
+     value = read(COLORS);   //gets input from keyboard as an integer
+     temp = int_Conversion_to_Char(value); //global variable that saves the chosen color
+     return true; //fittizio
+}
+
+void fn_ELABORATE(void){
+/*	if(elaborate()){ current_state = INSERT_COLOR; }
+	else { current_state = WAIT; }*/
+	current_state = (elaborate()) ? INSERT_COLOR : WAIT;
+}
+
+void fn_INSERT_COLOR(){
+	insert_color(temp);  // calls insert_color(char color) function
+ //verifies if the array is full or not and based on that decides in which state to jump
+    current_state = (is_full()) ? WAIT_FULL : WAIT;
+/*	if(is_full()){ current_state= WAIT_FULL;} 
+    else { current_state= WAIT; }*/
+}
+
+void fn_ELIMINATE_COLOR(void){
+	eliminate_color();   //calls function eliminate_color
+	current_state= WAIT;  //changes the current_state to WAIT
+}
+
+
+
+
---
+++ src/gamelogic2.h
@@ -0,0 +1,34 @@
+#ifndef GAMELOGIC2_H
+#define GAMELOGIC2_H
+
+#include "stdio.h"
+#include "stdlib.h"
+#include "time.h"
+#include "stdbool.h"
+#include "fsm.h"
+#include "gamelogic1.h"
+
+#define OPTIONS 3  //number of button options: 1=input, 2=confirm, 3=back
+char temp = '\0';  // used to pass converted vale into char(color) crom a function to another
+int value = 0;   //used to pass input value from a function to another
+bool selection = true; // true if text_selection -> buttons menu, false if text selection -> colors menu
+
+/* Function declarations */
+void fn_WAIT(void);                 //Function for WAIT state
+void fn_ELABORATE(void);            //Function for ELABORATE state
+void fn_INSERT_COLOR(void);         //Function for INSERT_COLOR state
+void fn_WAIT_FULL(void);            //Function for WAIT_FULL state
+void fn_ELIMINATE_COLOR(void);      //Function for ELIMINATE_COLOR state
+bool is_empty(void);                // checks if the array seq_user  is empty
+bool is_full(void);                 // checks if the array seq_user  is full
+void text_selection(bool sel);      //selects the test to display
+void full_text();                  //displays the text if the arrya is full
+int read(int dim);        //reads input value from keyboard and verifies if correct
+void handle_wait(int value);      //menu selection(buttons or colors)
+void wait(void);   //Function that waits for an input from the buttons(interrupt)
+void wait_full(void);     //Function to wait if the array is full 
+void insert_color(char color);   //Function to insert color in the array seq_user
+void eliminate_color(void);   //Function to eliminate color from the array seq_user
+bool elaborate(void);   //Function to verify if the input is valid (work in progress)
+
+#endif
