4cf6acc4b59b0d833659d312e553351708415de0
Gotta003 <94313973+Gotta003@users.noreply.github.com>
2024-10-10 18:29:01 +0200

    Create difficulty.h

---
+++ general/difficulty.h
@@ -0,0 +1,21 @@
+/*
+ * difficulty.h
+ *
+ *  Created on: 10 ott 2024
+ *      Author: matteogottardelligmail.com
+ */
+
+#ifndef DIFFICULTY_H_
+#define DIFFICULTY_H_
+#include "stdio.h"
+#include "stdlib.h"
+#include "implementations.h"
+#define DIFF_DIM 20
+#define DIFF_TYPES 3
+
+void fn_DIFFICULTY(void);
+void drawDifficulty(int32_t elems);
+
+extern Graphics_Button diff_buttons[];
+extern Graphics_Text explain[][DIFF_TYPES];
+#endif /* DIFFICULTY_H_ */
