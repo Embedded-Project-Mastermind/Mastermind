cc4ca26ca6c533f48004c62f257f03dbff70a0ff
Gotta003 <94313973+Gotta003@users.noreply.github.com>
2024-10-10 12:49:34 +0200

    Update implementations.h

--- general/implementations.h
+++ general/implementations.h
@@ -11,11 +11,24 @@
 #include "stdlib.h"
 #include "graphics.h"
 
-void graphics_Init();
-void hardware_Init();
-void reset_Screen();
+void graphics_Init(void);
+void hardware_Init(void);
+void reset_Screen(void);
 //FOLLOWING DEFINE THE GRAPHIC FUNCTIONS USED IN C FILE
-
+extern Graphics_Rectangle upperRect;
+extern Graphics_Button prevButton;
+extern Graphics_Button nextButton;
+extern Graphics_Text labelText;
+extern uint8_t position;
+//FIND POSITION RECT
+int32_t getCenteredX(Graphics_Rectangle rect);
+int32_t getCenteredY(Graphics_Rectangle rect);
+//COLOR CHOICE FUNCTIONS
+int32_t chooseColorRect(Button_State state, int32_t color);
+int32_t chooseColorText(Button_State state, int32_t color);
+//FUNCTIONS DRAW
+void defaultDraw(void);
+void drawDimension(void);
 //FUNCTIONS FOR STATES
 void fn_START_GR(void);
 void fn_DIMENSION(void);
