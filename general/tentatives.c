563e4ab00942a9b8bd7949696098ddc48958543d
niccolocristoforetti <113518101+niccolocristoforetti@users.noreply.github.com>
2024-10-10 18:24:15 +0200

    Update tentatives.c

--- general/tentatives.c
+++ general/tentatives.c
@@ -9,8 +9,13 @@
 #include "graphics.h"
 
 void drawTentatives(int elems) {
+    Graphics_Rectangle temp={1, 32+2, TENT_DIM-1, 32+TENT_DIM-1};
+    Graphics_drawRectangle(&grContext, &temp);
+    Graphics_setForegroundColor(&grContext, GRAPHICS_COLOR_BLACK);
+    Graphics_fillRectangle(&grContext, &temp);
+    drawButton(text_no_tent, STANDARD_COLOR, SELECTED_COLOR);
     int i;
-       for (i=0; i<elems; i++) {
+    for (i=0; i<elems; i++) {
            if (i<elems-2) {
                drawButton(tent_buttons[i], STANDARD_COLOR, SELECTED_COLOR);
            }
@@ -24,11 +29,12 @@ void fn_TENTATIVE(void) {
     reset_Screen();
     //DRAW FUNCTION
     reset_Screen();
-    int32_t num_elements=9; //(sizeof(diff_buttons)/sizeof(diff_buttons[0]));
+    int32_t num_elements=8; //(sizeof(diff_buttons)/sizeof(diff_buttons[0]));
     tent_buttons[num_elements-2]=prevButton;
     tent_buttons[num_elements-1]=nextButton;
     defaultDraw();
     drawTentatives(num_elements);
     //FOLLOWING POSITION
 }
+
 
