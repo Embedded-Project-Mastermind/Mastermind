6175d14800a6e55dd22dd690c7fce69b6b183fd9
Gotta003 <94313973+Gotta003@users.noreply.github.com>
2024-10-11 09:14:09 +0200

    Update difficulty.c

--- general/difficulty.c
+++ general/difficulty.c
@@ -7,11 +7,11 @@
 
 
 #include "difficulty.h"
-
-void drawDifficulty(int32_t elems) {
+#include "implementations.h"
+void drawDifficulty() {
     int i;
-    for (i=0; i<elems; i++) {
-        if (i<elems-2) {
+    for (i=0; i<sizes[DIFFICULTY]; i++) {
+        if (i<sizes[DIFFICULTY]-2) {
             drawButton(diff_buttons[i], STANDARD_COLOR, SELECTED_COLOR);
         }
         else {
@@ -20,19 +20,49 @@ void drawDifficulty(int32_t elems) {
     }
 }
 void drawText() {
+    Graphics_Rectangle temp={DIFF_DIM+2, 32+1, 128-1, 96-1};
+    Graphics_setForegroundColor(&grContext, STANDARD_COLOR);
+    Graphics_fillRectangle(&grContext, &temp);
     Graphics_setForegroundColor(&grContext, SELECTED_COLOR);
     int i;
-    for (i=0; i<4; i++) {//Logic implementation
+    for (i=0; i<DIFF_TEXT; i++) {//Logic implementation
         Graphics_drawString(&grContext, (int8_t *)explain[i][position].string, AUTO_STRING_LENGTH, DIFF_DIM+10,  32+10+10*i, explain[i][position].opacity);
     }
 }
 
 void fn_DIFFICULTY(void) {
     reset_Screen();
-    int32_t num_elements=5; //Logic implementation
-    diff_buttons[num_elements-2]=prevButton;
-    diff_buttons[num_elements-1]=nextButton;
+    diff_buttons[sizes[DIFFICULTY]-2]=prevButton;
+    diff_buttons[sizes[DIFFICULTY]-1]=nextButton;
     defaultDraw();
-    drawDifficulty(num_elements);
+    drawDifficulty();
     drawText();
+}
+void upStick_DIFFICULTY() {
+    if(position>0 && position<sizes[DIFFICULTY]-2) {
+        handleOut(diff_buttons, position);
+        position--;
+        handleIn(diff_buttons, position);
+        drawText();
+    }
+    else if (position>=sizes[DIFFICULTY]-2) {
+        handleOut(diff_buttons, position);
+        position=2;
+        handleIn(diff_buttons, position);
+        drawText();
+    }
+}
+void downStick_DIFFICULTY() {
+    if(position<3) {
+        handleOut(diff_buttons, position);
+        position++;
+        handleIn(diff_buttons, position);
+        drawText();
+    }
+}
+void leftStick_DIFFICULTY() {
+
+}
+void rightStick_DIFFICULTY() {
+
 }
