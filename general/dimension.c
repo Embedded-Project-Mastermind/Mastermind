a61283c1c7100a3671ed3a081c7c5b3a95e94c3f
ale-bena <127418373+ale-bena@users.noreply.github.com>
2024-10-11 12:27:27 +0200

    Update dimension.c

--- general/dimension.c
+++ general/dimension.c
@@ -7,10 +7,10 @@
 #include "dimension.h"
 #include "implementations.h"
 
-void drawDimension(int32_t elems) {
+void drawDimension() {
     int i;
-    for (i=0; i<elems; i++) {
-        if (i<elems-2) {
+    for (i=0; i<sizes[DIMENSION]; i++) {
+        if (i<sizes[DIMENSION]-2) {
             Graphics_setFont(&grContext, &g_sFontCm20);
             drawButton(dim_buttons[i], STANDARD_COLOR, SELECTED_COLOR);
         }
@@ -21,15 +21,84 @@ void drawDimension(int32_t elems) {
     }
 }
 
+void handleFont(){
+    if (position<sizes[DIMENSION]-2) {
+        Graphics_setFont(&grContext, &g_sFontCm20);
+    }
+    else{
+        Graphics_setFont(&grContext, &g_sFontFixed6x8);
+    }
+}
+
 void fn_DIMENSION(void) {
-    reset_Screen();
-
-    int32_t num_elements = 6;  // 6 elements, 4 buttons for dimension selection and back and next buttons
-    dim_buttons[num_elements-2]=prevButton; //add prev to the array
-    dim_buttons[num_elements-1]=nextButton;  //add back to the array
+    reset_Screen();  //screen reset function
+//    int32_t num_elements = 6;  // 6 elements, 4 buttons for dimension selection and back and next buttons
+    dim_buttons[sizes[DIMENSION]-2]=prevButton; //add prev to the array
+    dim_buttons[sizes[DIMENSION]-1]=nextButton;  //add back to the array
     defaultDraw(); //default draw function, draws top part, back and next buttons
-
-    drawDimension(num_elements);
+    drawDimension();
     //FOLLOWING POSITION
 }
+
+void upStick_DIMENSION(){
+    if(position>1){
+        handleFont();
+        handleOut(dim_buttons, position, sizes[DIMENSION]);
+        position=position-2;
+        handleFont(); //sarebbe da ottimizzare
+        handleIn(dim_buttons, position);
+    }
+}
+
+void downStick_DIMENSION(){
+    if(position<3){
+            handleFont();
+            handleOut(dim_buttons, position, sizes[DIMENSION]);
+            position=position+2;
+            handleFont();
+            handleIn(dim_buttons, position);
+    }
+    else if(position==3 && dim_buttons[sizes[DIMENSION]-1].state!=DISABLED ){
+        handleFont();
+        handleOut(dim_buttons, position, sizes[DIMENSION]);
+        position=position+2;
+        handleFont();
+        handleIn(dim_buttons, position);
+    }
+}
+
+void leftStick_DIMENSION(){
+    //int num=position%2;
+    if((position%2)==1){
+        handleFont();
+        handleOut(dim_buttons, position, sizes[DIMENSION]);
+        position--;
+        handleFont();
+        handleIn(dim_buttons, position);
+    }
+}
+
+void rightStick_DIMENSION(){
+    //int num=position%2;
+    if((position%2)==0){
+        if(position<4){
+            handleFont();
+            handleOut(dim_buttons, position, sizes[DIMENSION]);
+            position++;
+            handleFont();
+            handleIn(dim_buttons, position);
+        }
+        if(position==4 && dim_buttons[sizes[DIMENSION]-1].state!=DISABLED){
+           handleFont();
+           handleOut(dim_buttons, position, sizes[DIMENSION]);
+           position++;
+           handleFont();
+           handleIn(dim_buttons, position);
+       }
+     }
+}
+
+
+
+
 
