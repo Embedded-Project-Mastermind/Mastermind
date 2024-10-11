345de85a68ed2a2420e64db51d6bc326da34c6f5
Gotta003 <94313973+Gotta003@users.noreply.github.com>
2024-10-11 09:13:30 +0200

    Create buttons.h

---
+++ general/buttons.h
@@ -0,0 +1,14 @@
+/*
+ * buttons.h
+ *
+ *  Created on: 11 ott 2024
+ *      Author: matteogottardelligmail.com
+ */
+
+#ifndef BUTTONS_H_
+#define BUTTONS_H_
+
+void button_Init();
+void buttonBackAction();
+void buttonConfirmAction();
+#endif /* BUTTONS_H_ */
