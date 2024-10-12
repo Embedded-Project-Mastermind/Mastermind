41f7b5c7d1732e8887a1501df6e4b31640c5b1b5
Gotta003 <94313973+Gotta003@users.noreply.github.com>
2024-10-12 13:59:37 +0200

    Update start.h

--- general/start.h
+++ general/start.h
@@ -13,13 +13,22 @@
 #include "graphics.h"
 #include "bitmap.h"
 
-#define CHRONO_DIM 32
+#define UPPER_LEFT_X 18
+#define UPPER_LEFT_Y 16
 
 extern Graphics_Button start_button;
 
 void drawStart(int elems);
 void fn_START_GR(void);
-void drawPixel(uint32_t x, uint32_t y, const uint8_t color);
+uint16_t encodeColor(uint8_t r, uint8_t g, uint8_t b);
 //void drawImage(uint32_t x, uint32_t y);
+static const Graphics_Image grImage={
+      .bPP=IMAGE_FMT_8BPP_UNCOMP,
+      .numColors=256,
+      .pPalette=(const uint32_t *)palette,
+      .pPixel=(const uint8_t *)image,
+      .xSize=SIZE_IMAGE_X,
+      .ySize=SIZE_IMAGE_Y
 
+};
 #endif /* START_H_ */
