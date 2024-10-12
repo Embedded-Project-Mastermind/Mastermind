2826e4cb64f5f2d05a3159e782f6384ef5447126
Gotta003 <94313973+Gotta003@users.noreply.github.com>
2024-10-12 13:58:00 +0200

    Update info.c

--- general/info.c
+++ general/info.c
@@ -14,24 +14,28 @@ void change_screen_i(void){
     info_buttons[1]=nextButton;
 
     char buffer[sizeof(long)*8+1][2];
-    game.dim=3;
+    /*game.dim=3;
     game.tentatives=6;
-    game.difficulty= 'B';
-     int8_t str[2]={game.difficulty,'\0'};
+    game.difficulty= 'B';*/
+    int8_t str[2]={game.difficulty,'\0'};
     info_texts_results[0].string= (int8_t*)ltoa((long)game.dim, buffer[0], 10);
     info_texts_results[1].string= str;
     info_texts_results[2].string= (game.doubles) ? (int8_t*)"Yes" : (int8_t*)"No";
     info_texts_results[3].string= (int8_t*)ltoa((long)game.tentatives, buffer[1], 10);
 
-    Graphics_setForegroundColor(&grContext, SELECTED_COLOR);
+    Graphics_setForegroundColor(&grContext, GRAPHICS_COLOR_BLACK);
        int i, elems= 4;
        for(i=0; i< elems; i++){ Graphics_drawString(&grContext, (int8_t *)info_texts[i].string, AUTO_STRING_LENGTH, 10 , 40+10*i , info_texts[i].opacity);}
        for(i=0; i< elems; i++){ Graphics_drawString(&grContext, (int8_t *)info_texts_results[i].string, AUTO_STRING_LENGTH, 90 , 40+10*i , info_texts_results[i].opacity);}
     Graphics_setForegroundColor(&grContext, FILL_UPPER_RECT);
 
-    for(i=0; i<2; i++){drawButton(info_buttons[i], FILL_MOVEMENT, STANDARD_COLOR);}
+    for(i=0; i<2; i++){drawButton(info_buttons[i], FILL_MOVEMENT, STANDARD_COLOR, position);}
 }
 void fn_INFO(void) {
+    reset_Screen();
+    position=sizes[INFO];
+    info_buttons[sizes[INFO]-1].state=STANDARD;
+    initArray(info_buttons, sizes[INFO]);
     change_screen_i();
 }
 
