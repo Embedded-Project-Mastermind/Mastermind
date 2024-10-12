cc625dfb3b7460433bba3acf32247a85658081d5
Gotta003 <94313973+Gotta003@users.noreply.github.com>
2024-10-12 13:57:34 +0200

    Update implementations.c

--- general/implementations.c
+++ general/implementations.c
@@ -8,6 +8,11 @@
 #include "graphics.h"
 #include "joystick.h"
 #include "buttons.h"
+#include "dimension.h"
+#include "difficulty.h"
+#include "tentatives.h"
+#include "doubles.h"
+
 
 void reset_Screen(void) {
     Graphics_setFont(&grContext, &g_sFontFixed6x8); //resets the font to the standard one, probably not necessary but anyways it's good practice to put it
@@ -33,7 +38,7 @@ int32_t getCenteredY(Graphics_Rectangle rect) {
 int32_t chooseColorRect(Button_State state, int32_t color) {
     int32_t c;
     switch(state) {
-        case FOCUSED: c=color; break;
+        case FOCUSED: c=STANDARD_COLOR; break;
         case STANDARD: c=color; break;
         case SELECTED: c=SELECTED_COLOR; break;
         case DISABLED: c=DISABLED_COLOR; break;
@@ -52,18 +57,34 @@ int32_t chooseColorText(Button_State state, int32_t color) {
     }
     return c;
 }
-void focusedhandle(Graphics_Rectangle rect, int32_t color) {
+void focusedhandle(Button_State state, Graphics_Rectangle rect, int32_t color) {
     Graphics_drawRectangle(&grContext, &rect);
     Graphics_setForegroundColor(&grContext, BORDER_FOCUSED_COLOR);
     Graphics_fillRectangle(&grContext, &rect);
     Graphics_Rectangle temp={rect.xMin+BORDER, rect.yMin+BORDER, rect.xMax-BORDER, rect.yMax-BORDER};
     Graphics_drawRectangle(&grContext, &temp);
-    Graphics_setForegroundColor(&grContext, chooseColorRect(FOCUSED, color));
+    if (state==FOCUSED) {
+        Graphics_setForegroundColor(&grContext, chooseColorRect(FOCUSED, color));
+    }
+    else {
+        if (state==SELECTED) {
+            Graphics_setForegroundColor(&grContext, chooseColorRect(SELECTED, color));
+        }
+    }
     Graphics_fillRectangle(&grContext, &temp);
 }
-void drawButton(Graphics_Button button, int32_t rect_color, int32_t text_color) {
-    if(button.state==FOCUSED) {
-        focusedhandle(button.rect, rect_color);
+int8_t findSelected(Graphics_Button array[], int8_t size) {
+    int8_t i;
+    for (i=0; i<size; i++) {
+        if (array[i].state==SELECTED) {
+            return i;
+        }
+    }
+    return -1;
+}
+void drawButton(Graphics_Button button, int32_t rect_color, int32_t text_color, int8_t pos_selected) {
+    if(button.state==FOCUSED || (button.state==SELECTED && position==pos_selected)) {
+        focusedhandle(button.state, button.rect, rect_color);
     }
     else {
         Graphics_drawRectangle(&grContext, &button.rect);
@@ -85,17 +106,29 @@ void defaultDraw(void) {
     }
 }
 void handleOut(Graphics_Button array[], uint8_t position, int8_t size) {
-    array[position].state=STANDARD;
     if(position<(size-2)) {
-        drawButton(array[position], STANDARD_COLOR, SELECTED_COLOR);
+        if (array[position].state!=SELECTED) {
+            array[position].state=STANDARD;
+            drawButton(array[position], STANDARD_COLOR, SELECTED_COLOR, findSelected(array, size));
+        }
+        else {
+            int output=findSelected(array, size)+1;//IN ORDER TO MAKE output!=position
+            drawButton(array[position], SELECTED_COLOR, STANDARD_COLOR, output);
+        }
     }
     else {
-        drawButton(array[position], FILL_MOVEMENT, STANDARD_COLOR);
+        array[position].state=STANDARD;
+        drawButton(array[position], FILL_MOVEMENT, STANDARD_COLOR, findSelected(array, size));
     }
 }
-void handleIn(Graphics_Button array[], uint8_t position) {
-    array[position].state=FOCUSED;
-    drawButton(array[position], STANDARD_COLOR, BORDER_FOCUSED_COLOR);
+void handleIn(Graphics_Button array[], uint8_t position, int8_t size) {
+    if (array[position].state!=SELECTED) {
+        array[position].state=FOCUSED;
+        drawButton(array[position], STANDARD_COLOR, BORDER_FOCUSED_COLOR, findSelected(array, size));
+    }
+    else {
+        drawButton(array[position], SELECTED_COLOR, STANDARD_COLOR, findSelected(array, size));
+    }
 }
 void hardware_Init() {
     before_ADC();
@@ -106,18 +139,51 @@ void hardware_Init() {
 
 void handle_buttons(Graphics_Button array[]){
     if(position<(sizes[display_position]-2)){
-        array[position].state=SELECTED;
+        switch(display_position) {
+            case DIMENSION: handlePressure_DIMENSION(); break;
+            case DIFFICULTY: handlePressure_DIFFICULTY(); break;
+            case TENTATIVE: handlePressure_TENTATIVE(); break;
+            case DOUBLES: handlePressure_DOUBLES(); break;
+            default: exit(1);
         }
+    }
     else{
        if(position==(sizes[display_position]-2)){
            display_position--;
+           position=0;
        }
-       else if(position==(sizes[display_position]-1)){
-           display_position++;
+       else {
+           if(position==(sizes[display_position]-1)){
+               display_position++;
+               position=0;
+           }
        }
     }
 }
+void handleSelection(Graphics_Button array[]) {
+    int i;
+    for (i=0; i<sizes[display_position]; i++) {
+        array[i].state=STANDARD;
+    }
+    array[position].state=SELECTED;
+    position=sizes[display_position]-1;
+    array[position].state=FOCUSED;
 
+}
+void initArray(Graphics_Button array[], int size) {
+    if (position!=size-2) {
+        array[size-2]=prevButton;
+    }
+    if (position!=size-1) {
+        array[size-1]=nextButton;
+    }
+    if(findSelected(array, size)!=-1) {
+        array[size-1].state=STANDARD;
+    }
+    if (position!=findSelected(array, size)) {
+        array[position].state=FOCUSED;
+    }
+}
 void fn_GAME(void) {
     reset_Screen();
     //DRAW FUNCTION
