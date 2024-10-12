170fa9fd0eba06402e394a6f5a5b9d7d8867a39e
Gotta003 <94313973+Gotta003@users.noreply.github.com>
2024-10-12 13:56:47 +0200

    Update doubles.h

--- general/doubles.h
+++ general/doubles.h
@@ -12,6 +12,7 @@ extern Graphics_Text doubles_text;
 extern Graphics_Text doubles_description[];
 extern Graphics_Button doubles_buttons[];
 
+void handlePressure_DOUBLES();
 void create_doubles_text(void);
 void create_doubles_button(void);
 void change_screen_d(void); //to call the other two functions and set minor things
@@ -20,5 +21,6 @@ void upStick_DOUBLES();
 void downStick_DOUBLES();
 void leftStick_DOUBLES();
 void rightStick_DOUBLES();
+void buttons_DOUBLES();
 
 #endif /* DOUBLES_H_ */
