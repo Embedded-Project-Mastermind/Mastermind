0d02abc20739117749b168e4d434df728dcc0714
Gotta003 <94313973+Gotta003@users.noreply.github.com>
2024-10-29 10:41:50 +0100

    Update difficulty.c

--- general/difficulty.c
+++ general/difficulty.c
@@ -8,6 +8,14 @@
 
 #include "difficulty.h"
 #include "implementations.h"
+
+Graphics_Text explain[][DIFF_TYPES]={
+     {{"EASY MODE", false},{"MEDIUM MODE", false}, {"HARD MODE", false}},
+     {{"INFO PER COLOR", false},{"INFO GENERAL", false},{"INFO GENERAL", false}},
+     {{"- RIGHT POS", false},{"- RIGHT POS", false},{"- RIGHT POS", false}},
+     {{"- WRONG POS", false}, {"- WRONG POS", false}, {"", false}}
+};
+
 void drawDifficulty() {
     int i;
     for (i=0; i<sizes[DIFFICULTY]; i++) {
@@ -24,10 +32,18 @@ void drawText() {
     Graphics_setForegroundColor(&grContext, STANDARD_COLOR);
     Graphics_fillRectangle(&grContext, &temp);
     Graphics_setForegroundColor(&grContext, SELECTED_COLOR);
-    if (position<sizes[DIFFICULTY]-1) {
-        int i;
+    int8_t i;
+    if (position<sizes[DIFFICULTY]-2) {
         for (i=0; i<DIFF_TEXT; i++) {//Logic implementation
             Graphics_drawString(&grContext, (int8_t *)explain[i][position].string, AUTO_STRING_LENGTH, DIFF_DIM+10,  32+10+10*i, explain[i][position].opacity);
+        }
+    }
+    else {
+        int8_t selected=findSelected(diff_buttons, sizes[DIFFICULTY]);
+        if (selected>=0) {
+            for (i=0; i<DIFF_TEXT; i++) {
+                Graphics_drawString(&grContext, (int8_t *)explain[i][(int) selected].string, AUTO_STRING_LENGTH, DIFF_DIM+10,  32+10+10*i, explain[i][selected].opacity);
+            }
         }
     }
 }
@@ -68,6 +84,7 @@ void leftStick_DIFFICULTY() {
         handleOut(diff_buttons, position, sizes[DIFFICULTY]);
         position--;
         handleIn(diff_buttons, position, sizes[DIFFICULTY]);
+        drawText();
     }
 }
 void rightStick_DIFFICULTY() {
@@ -75,7 +92,7 @@ void rightStick_DIFFICULTY() {
             handleOut(diff_buttons, position, sizes[DIFFICULTY]);
             position++;
             handleIn(diff_buttons, position, sizes[DIFFICULTY]);
-
+            drawText();
       }
 }
 
