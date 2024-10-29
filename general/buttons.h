c1629791fe4befc2cf87f55ac6f0250e7749d861
Gotta003 <94313973+Gotta003@users.noreply.github.com>
2024-10-29 10:42:18 +0100

    Update buttons.h

--- general/buttons.h
+++ general/buttons.h
@@ -7,8 +7,11 @@
 
 #ifndef BUTTONS_H_
 #define BUTTONS_H_
+#include "fsm.h"
+#define DEBOUNCE_TIME 20
 
 void button_Init();
 void buttonBackAction();
 void buttonConfirmAction();
+void functionPinsDefault(uint8_t status, int8_t port);
 #endif /* BUTTONS_H_ */
