98aaa3941baeeb6974af2369ff269cd91edf16c6
Gotta003 <94313973+Gotta003@users.noreply.github.com>
2024-10-29 10:39:51 +0100

    Create game.h

---
+++ general/game.h
@@ -0,0 +1,19 @@
+/*
+ * game.h
+ *
+ *  Created on: 27 ott 2024
+ *      Author: matteogottardelligmail.com
+ */
+
+#ifndef GAME_H_
+#define GAME_H_
+#include "graphics.h"
+#include "implementations.h"
+#include "input.h"
+
+extern bool configurationGame;
+
+void fn_GAME(void);
+void elaborateColor(int8_t c);
+
+#endif /* GAME_H_ */
