906bc3a06491708f4f212ea5e0a5c1d512324912
Kagenshi <46927892+Kagenshi@users.noreply.github.com>
2024-10-30 16:57:00 +0100

    Update doubles.h

--- general/doubles.h
+++ general/doubles.h
@@ -8,15 +8,20 @@
 #ifndef DOUBLES_H_
 #define DOUBLES_H_
 #include "implementations.h"
+// arrays with buttons and texts of this interface.
+// Previously implemented in the main
 extern Graphics_Text doubles_text;
 extern Graphics_Text doubles_description[];
 extern Graphics_Button doubles_buttons[];
 
-void handlePressure_DOUBLES();
+// functions needed to create the interface
 void create_doubles_text(void);
 void create_doubles_button(void);
 void change_screen_d(void); //to call the other two functions and set minor things
 void fn_DOUBLES(void);
+
+// functions needed to interact with the interface
+void handlePressure_DOUBLES();
 void upStick_DOUBLES();
 void downStick_DOUBLES();
 void leftStick_DOUBLES();
