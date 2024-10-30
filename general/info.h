567b936dca030d5c9bf48414c492ea9fa72affe5
Kagenshi <46927892+Kagenshi@users.noreply.github.com>
2024-10-30 16:58:35 +0100

    Update info.h

--- general/info.h
+++ general/info.h
@@ -8,16 +8,22 @@
 #ifndef INFO_H_
 #define INFO_H_
 #include "implementations.h"
+// arrays with buttons and texts of this interface.
+// Previously implemented in the main
 extern Graphics_Text info_texts[];
 extern Graphics_Text info_texts_results[];
 extern Graphics_Button info_buttons[];
 
+//functions needed to create the interface
 void change_screen_i(void);
 void fn_INFO(void);
+
+//functions needed to interact with the interface
 void upStick_INFO();
 void downStick_INFO();
 void leftStick_INFO();
 void rightStick_INFO();
+void handlePressure_INFO();
 void buttons_INFO();
 
 #endif /* INFO_H_ */
