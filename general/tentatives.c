44f36af2812c0cfd4cf33239d0afa65fd5256b34
niccolocristoforetti <113518101+niccolocristoforetti@users.noreply.github.com>
2024-10-30 23:47:37 +0100

    Update tentatives.c

--- general/tentatives.c
+++ general/tentatives.c
@@ -8,7 +8,16 @@
 #include "implementations.h"
 #include "graphics.h"
 
-void drawTentatives(int elems) {
+/***************************************
+*
+* drawTentatives() - function that create
+* buttons and text for the Tentatives screen
+* no input
+* no output
+*
+***************************************/
+
+void drawTentatives(void) {
     Graphics_Button other_things[]={
         {DISABLED, {TENT_DIM+1, 32+2, TENT_DIM*4-1, 32+TENT_DIM-1}, {"NO TENT", false}},
         {DISABLED, {1, 32+2, TENT_DIM-1, 32+TENT_DIM-1}, {"", false}}
@@ -17,8 +26,8 @@ void drawTentatives(int elems) {
     for (i=0; i<2; i++) {
         drawButton(other_things[i], STANDARD_COLOR, SELECTED_COLOR, findSelected(tent_buttons, sizes[TENTATIVE]));
     }
-    for (i=0; i<elems; i++) {
-           if (i<elems-2) {
+    for (i=0; i<sizes[TENTATIVE]; i++) {
+           if (i<sizes[TENTATIVE]-2) {
                drawButton(tent_buttons[i], STANDARD_COLOR, SELECTED_COLOR, findSelected(tent_buttons, sizes[TENTATIVE]));
            }
            else {
@@ -27,15 +36,37 @@ void drawTentatives(int elems) {
        }
 }
 
+/***************************************
+*
+* fn_TENTATIVE() - functions that create
+* the tentatives screen starting from
+* resetting the previous screen
+* no input
+* no output
+*
+***************************************/
+
+
 void fn_TENTATIVE(void) {
     reset_Screen();
     //DRAW FUNCTION
     reset_Screen();
     initArray(tent_buttons, sizes[TENTATIVE]);
     defaultDraw();
-    drawTentatives(sizes[TENTATIVE]);
+    drawTentatives();
     //FOLLOWING POSITION
 }
+
+/***************************************
+*
+* upStick_TENTATIVE() / douwnStick_TENTATIVE() / leftStick_TENTATIVE() / rightStick_TENTATIVE()
+* they manage what to do when the user
+* moves the stick
+* no input
+* no output
+*
+***************************************/
+
 void upStick_TENTATIVE() {
     if(position>=1 && position<=4) {
         handleOut(tent_buttons, position, sizes[TENTATIVE]);
@@ -111,6 +142,17 @@ void rightStick_TENTATIVE() {
          }
     }
 }
+
+/***************************************
+*
+* handlePressure_TENTATIVE() - manages the choice
+* of tentative trough the pressure of the
+* button (except to prev and next)
+* no input
+* no output
+*
+***************************************/
+
 void handlePressure_TENTATIVE() {
     if (position!=0) {
         game.tentatives=atoi((char*)tent_buttons[position].text.string);
