a5d13e64680d2e5338b2c7ffeca5f5272161c5bc
ale-bena <127418373+ale-bena@users.noreply.github.com>
2024-10-31 15:26:27 +0100

    Update gamelogic2.h

--- general/gamelogic2.h
+++ general/gamelogic2.h
@@ -13,6 +13,7 @@
 #include "stdbool.h"
 #include "fsm.h"
 #include "gamelogic1.h"
+#include "game.h"
 
 #define OPTIONS 3  //number of button options: 1=input, 2=confirm, 3=back
 
@@ -26,12 +27,12 @@ bool is_empty(void);                // checks if the array seq_user  is empty
 bool is_full(void);                 // checks if the array seq_user  is full
 void text_selection(bool sel);      //selects the test to display
 void full_text(void);                  //displays the text if the arrya is full
-int read_input(int dim);        //reads input value from keyboard and verifies if correct
-void handle_wait(int value);      //menu selection(buttons or colors)
-void print_sequence(char* seq, int dim);  //to print the sequence
+int read_input(int16_t dim);        //reads input value from keyboard and verifies if correct
+void handle_wait(int16_t value);      //menu selection(buttons or colors)
+void print_sequence(int8_t* seq, int16_t dim);  //to print the sequence
 void wait_handle(void);   //Function that waits for an input from the buttons(interrupt)
 void wait_full(void);     //Function to wait if the array is full 
-void insert_color(char color);   //Function to insert color in the array seq_user
+void insert_color();   //Function to insert color in the array seq_user
 void eliminate_color(void);   //Function to eliminate color from the array seq_user
 bool elaborate(void);   //Function to verify if the input is valid (work in progress)
 
