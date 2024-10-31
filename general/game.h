38b9e85466c1a3542ffa232b525f5ba5bb6ee873
Gotta003 <94313973+Gotta003@users.noreply.github.com>
2024-10-31 13:51:41 +0100

    Update game.h

--- general/game.h
+++ general/game.h
@@ -12,9 +12,14 @@
 #include "input.h"
 
 extern bool configurationGame;
-extern int16_t color_selected;
+
+extern Graphics_Circle circles[];
 
 void fn_GAME(void);
 void elaborateColor(int8_t c);
+void initCircles();
+void populateCircles(int16_t offset, int16_t radius);
+void drawCircles();
+
 
 #endif /* GAME_H_ */
