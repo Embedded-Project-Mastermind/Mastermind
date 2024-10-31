5f265bd8ce2c561ec4995e96146c99daa1ce98f3
niccolocristoforetti <113518101+niccolocristoforetti@users.noreply.github.com>
2024-10-31 15:07:06 +0100

    Update chronology.h

--- general/chronology.h
+++ general/chronology.h
@@ -15,13 +15,16 @@
 
 extern Graphics_Button chronology_buttons[];
 extern Graphics_Button other_buttons[];
+extern Graphics_Button tent_number[];
 Graphics_Button return_to;
+int16_t pos; //var for manage the tent outputs after having overtaken three tentative
 
 void drawChronology(void);
 void fn_CHRONOLOGY(void);
 void upStick_CHRONOLOGY();
 void downStick_CHRONOLOY();
 void rightStick_CHRONOLOGY();
+void updatePos();
 
 
 #endif /* CHRONOLOGY_H_ */
