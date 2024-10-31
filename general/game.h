5005fb3e23816f5e2bab10daaf08fc4e8386a7bc
ale-bena <127418373+ale-bena@users.noreply.github.com>
2024-10-31 14:38:36 +0100

    Update game.h

--- general/game.h
+++ general/game.h
@@ -16,7 +16,7 @@ extern bool configurationGame;
 extern Graphics_Circle circles[];
 
 void fn_GAME(void);
-void elaborateColor(int8_t c);
+void elaborateColor(int16_t c);
 void initCircles();
 void populateCircles(int16_t offset, int16_t radius);
 void drawCircles();
