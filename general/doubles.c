42f72a37527cc59b9fb08e53438e11248866eecc
Gotta003 <94313973+Gotta003@users.noreply.github.com>
2024-11-05 19:24:25 +0100

    Update doubles.c

--- general/doubles.c
+++ general/doubles.c
@@ -7,7 +7,14 @@
  */
 
 #include "doubles.h"
-
+/***************************************
+*
+* create_doubles_text() - initializes the
+* texts and draw them
+* no input
+* no output
+*
+***************************************/
 void create_doubles_text(void){
     Graphics_setForegroundColor(&grContext, SELECTED_COLOR);
     Graphics_drawStringCentered(&grContext, (int8_t *)doubles_text.string, AUTO_STRING_LENGTH, 40 , 47 , doubles_text.opacity);
@@ -16,7 +23,15 @@ void create_doubles_text(void){
         Graphics_drawStringCentered(&grContext, (int8_t *)doubles_description[i].string, AUTO_STRING_LENGTH, 65 , 68+10*i , doubles_description[i].opacity);
     }
 }
-
+/***************************************
+*
+* create_doubles_buttons() - initializes the
+* buttons, putting them on an ordered array
+* and drawing them
+* no input
+* no output
+*
+***************************************/
 void create_doubles_buttons(void){
     int i, elems= 3;
     for(i=0; i<elems; i++){
@@ -29,7 +44,15 @@ void create_doubles_buttons(void){
     }
 
 }
-
+/***************************************
+*
+* change_screen_d - manages how to set
+* all doubles' interface, also calling the
+* functions to create the texts and the buttons
+* no input
+* no output
+*
+***************************************/
 void change_screen_d(void){
     rectangleWithText(upperRect, FILL_UPPER_RECT, labelText, SELECTED_COLOR);
     rectangleWithText((Graphics_Rectangle){1, 34, 126, 59}, DISABLED_COLOR, (Graphics_Text){{""}, false}, STANDARD_COLOR);
@@ -37,7 +60,15 @@ void change_screen_d(void){
     create_doubles_text();
     create_doubles_buttons();
 }
-
+/***************************************
+*
+* fn_DOUBLES() - calls all needed
+* functions to change the previous interface
+* with the one of the doubles' state
+* no input
+* no output
+*
+***************************************/
 void fn_DOUBLES(void) {
     reset_Screen();
     initArray(doubles_buttons, sizes[DOUBLES]);
@@ -46,7 +77,15 @@ void fn_DOUBLES(void) {
     }
     change_screen_d();
 }
-
+/***************************************
+*
+* upStick_DOUBLES() / douwnStick_DOUBLES() / leftStick_DOUBLES() / rightStick_DOUBLES()
+* they manage what to do when the user
+* moves the stick
+* no input
+* no output
+*
+***************************************/
 void upStick_DOUBLES(){
     if(position>0){
         handleOut(doubles_buttons, position, sizes[DOUBLES]);
@@ -75,6 +114,15 @@ void rightStick_DOUBLES(){
         handleIn(doubles_buttons, position, sizes[DOUBLES]);
        }
 }
+/***************************************
+*
+* handlePressure_DOUBLES() - manages the changes of
+* our doubles variable and the shown text on our
+* button (except to prev and next)
+* no input
+* no output
+*
+***************************************/
 void handlePressure_DOUBLES() {
     if (doubles_buttons[0].state == SELECTED) {
         game.doubles=false;
