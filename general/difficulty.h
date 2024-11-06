b7bb4a354ade01ddff6242f137b124fe36c7fa29
Kagenshi <46927892+Kagenshi@users.noreply.github.com>
2024-11-06 22:20:09 +0100

    Update difficulty.h

--- general/difficulty.h
+++ general/difficulty.h
@@ -15,14 +15,19 @@
 #define DIFF_TEXT 4
 #define DIFF_TYPES 4
 
+// functions needed to create the interface
 void fn_DIFFICULTY(void);
 void drawDifficulty();
+
+// functions needed to interact with the interface
 void upStick_DIFFICULTY();
 void downStick_DIFFICULTY();
 void leftStick_DIFFICULTY();
 void rightStick_DIFFICULTY();
 void handlePressure_DIFFICULTY();
 
+// arrays with buttons and texts of this interface.
+// Previously implemented in the main
 extern Graphics_Button diff_buttons[];
 extern Graphics_Text explain[DIFF_TEXT][DIFF_TYPES];
 #endif /* DIFFICULTY_H_ */
