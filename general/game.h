29cd00856df1ec547fc4cca20c9752ca140d4104
Gotta003 <94313973+Gotta003@users.noreply.github.com>
2024-11-07 10:01:39 +0100

    Update game.h

--- general/game.h
+++ general/game.h
@@ -6,22 +6,23 @@
  * Date Creation: 31 ott 2024
  */
 
-
 #ifndef GAME_H_
 #define GAME_H_
 #include "graphics.h"
 #include "implementations.h"
 #include "input.h"
-
+#define YLEVEL_OFFSET 32
 extern bool configurationGame;
-extern int16_t color_selected;
-extern Graphics_Circle circles[];
 
+extern Graphics_Circle circles[];
+extern Graphics_Rectangle textRect;
+extern int16_t color_selected;
 void fn_GAME(void);
 void elaborateColor(int16_t c);
 void initCircles();
 void populateCircles(int16_t offset, int16_t radius);
 void drawCircles();
-
-
+void leftStick_GAME();
+int32_t selectColor(int16_t color);
+void displayResultsOnScreen();
 #endif /* GAME_H_ */
