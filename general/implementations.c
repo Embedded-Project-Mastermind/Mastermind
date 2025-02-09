7fc064934b4f785e678f2c914f543a1db7a97f61
Kagenshi <46927892+Kagenshi@users.noreply.github.com>
2025-02-09 10:33:52 +0100

    Update implementations.c

--- general/implementations.c
+++ general/implementations.c
@@ -19,13 +19,28 @@
 #include "game.h"
 
 volatile bool mutex=false;
-
+/**
+ * @brief it resets all default's colors and clear the display
+ * @return void
+ * @author Matteo Gottardelli (Primary author)
+ * @author Niccolo Cristoforetti (Helper)
+ * @author Matteo Gottardelli (Maintainer)
+ * @date 2024-10-5
+ */
 void reset_Screen(void) {
     Graphics_setFont(&grContext, &g_sFontFixed6x8); //resets the font to the standard one, probably not necessary but anyways it's good practice to put it
     Graphics_setForegroundColor(&grContext, STANDARD_COLOR); //sets foreground to black
     Graphics_setBackgroundColor(&grContext, SELECTED_COLOR); //sets background to white for the borders
     Graphics_clearDisplay(&grContext);
 }
+/**
+ * @brief initializes the display and drivers
+ * @return void
+ * @author Matteo Gottardelli (Primary author)
+ * @author Niccolo Cristoforetti (Helper)
+ * @author Matteo Gottardelli (Maintainer)
+ * @date 2024-10-5
+ */
 void graphics_Init(void) {
     //INITIALIZE DISPLAY AND DRIVERS
     Crystalfontz128x128_Init();
@@ -35,12 +50,40 @@ void graphics_Init(void) {
     Graphics_setFont(&grContext, &g_sFontFixed6x8);
     reset_Screen();
 }
+/**
+ * @brief gives back the x of the center of the given rectangle
+ * @param the rectangle
+ * @return the x of the center of the given rectangle
+ * @author Matteo Gottardelli (Primary author)
+ * @author Niccolo Cristoforetti (Helper)
+ * @author Matteo Gottardelli (Maintainer)
+ * @date 2024-10-5
+ */
 int32_t getCenteredX(Graphics_Rectangle rect) {
     return (rect.xMax+rect.xMin)/2;
 }
+/**
+ * @brief gives back the y of the center of the given rectangle
+ * @param the rectangle
+ * @return the y of the center of the given rectangle
+ * @author Matteo Gottardelli (Primary author)
+ * @author Niccolo Cristoforetti (Helper)
+ * @author Matteo Gottardelli (Maintainer)
+ * @date 2024-10-5
+ */
 int32_t getCenteredY(Graphics_Rectangle rect) {
     return (rect.yMax+rect.yMin)/2;
 }
+/**
+ * @brief chooses the color of the button given its current state
+ * @param the current state of the button
+ * @param the current color of the button
+ * @return the choosen color
+ * @author Matteo Gottardelli (Primary author)
+ * @author Niccolo Cristoforetti (Helper)
+ * @author Matteo Gottardelli (Maintainer)
+ * @date 2024-10-5
+ */
 int32_t chooseColorRect(Button_State state, int32_t color) {
     int32_t c;
     switch(state) {
@@ -52,6 +95,16 @@ int32_t chooseColorRect(Button_State state, int32_t color) {
     }
     return c;
 }
+/**
+ * @brief chooses the color of the text on the button given the button's current state
+ * @param the current state of the button
+ * @param the current color of the text
+ * @return the choosen color
+ * @author Matteo Gottardelli (Primary author)
+ * @author Niccolo Cristoforetti (Helper)
+ * @author Matteo Gottardelli (Maintainer)
+ * @date 2024-10-5
+ */
 int32_t chooseColorText(Button_State state, int32_t color) {
     int32_t c;
     switch(state) {
@@ -63,6 +116,17 @@ int32_t chooseColorText(Button_State state, int32_t color) {
     }
     return c;
 }
+/**
+
+da capire e sistemare
+
+ * @brief initializes the display and drivers
+ * @return void
+ * @author Matteo Gottardelli (Primary author)
+ * @author Niccolo Cristoforetti (Helper)
+ * @author Matteo Gottardelli (Maintainer)
+ * @date 2024-10-5
+ */
 void focusedhandle(Button_State state, Graphics_Rectangle rect, int32_t color) {
     Graphics_drawRectangle(&grContext, &rect);
     Graphics_setForegroundColor(&grContext, BORDER_FOCUSED_COLOR);
@@ -79,6 +143,16 @@ void focusedhandle(Button_State state, Graphics_Rectangle rect, int32_t color) {
     }
     Graphics_fillRectangle(&grContext, &temp);
 }
+/**
+ * @brief finds the selected button in all the buttons of an interface
+ * @param the button's array
+ * @param the size of the array
+ * @return the position of the button in the "selected" state (if no button is selected, -1)
+ * @author Matteo Gottardelli (Primary author)
+ * @author Niccolo Cristoforetti (Helper)
+ * @author Matteo Gottardelli (Maintainer)
+ * @date 2024-10-5
+ */
 int8_t findSelected(Graphics_Button array[], int8_t size) {
     int8_t i;
     for (i=0; i<size; i++) {
@@ -88,6 +162,18 @@ int8_t findSelected(Graphics_Button array[], int8_t size) {
     }
     return -1;
 }
+/**
+ * @brief draws the button in the given position with the right color
+ * @param the button
+ * @param the button's color
+ * @param the button's text color
+ * @param then position
+ * @return void
+ * @author Matteo Gottardelli (Primary author)
+ * @author Niccolo Cristoforetti (Helper)
+ * @author Matteo Gottardelli (Maintainer)
+ * @date 2024-10-5
+ */
 void drawButton(Graphics_Button button, int32_t rect_color, int32_t text_color, int8_t pos_selected) {
     if(button.state==FOCUSED || (button.state==SELECTED && position==pos_selected)) {
         focusedhandle(button.state, button.rect, rect_color);
@@ -101,7 +187,18 @@ void drawButton(Graphics_Button button, int32_t rect_color, int32_t text_color, 
     Graphics_drawStringCentered(&grContext, (int8_t *) button.text.string, AUTO_STRING_LENGTH, getCenteredX(button.rect),  getCenteredY(button.rect), button.text.opacity);
 
 }
-
+/**
+ * @brief it draws a rectangle with a text centered in it
+ * @param
+ * @param
+ * @param
+ * @param
+ * @return void
+ * @author Matteo Gottardelli (Primary author)
+ * @author Niccolo Cristoforetti (Helper)
+ * @author Matteo Gottardelli (Maintainer)
+ * @date 2024-10-5
+ */
 void rectangleWithText(Graphics_Rectangle rect, int32_t colorRect, Graphics_Text text, int32_t colorText) {
     if(colorRect!=-1) {
         Graphics_drawRectangle(&grContext, &rect);
@@ -111,6 +208,17 @@ void rectangleWithText(Graphics_Rectangle rect, int32_t colorRect, Graphics_Text
     Graphics_setForegroundColor(&grContext, colorText); //Manual
     Graphics_drawStringCentered(&grContext, (int8_t *)text.string, AUTO_STRING_LENGTH, getCenteredX(rect),  getCenteredY(rect), text.opacity);
 }
+/**
+ * @brief initializes the display and drivers
+ * @param
+ * @param
+ * @param
+ * @return void
+ * @author Matteo Gottardelli (Primary author)
+ * @author Niccolo Cristoforetti (Helper)
+ * @author Matteo Gottardelli (Maintainer)
+ * @date 2024-10-5
+ */
 void handleOut(Graphics_Button array[], uint8_t position, int8_t size) {
     if(display_position==GAME || display_position==CHRONOLOGY){
          if (array[position].state!=SELECTED) {
@@ -139,6 +247,17 @@ void handleOut(Graphics_Button array[], uint8_t position, int8_t size) {
              }
     }
 }
+/**
+ * @brief initializes the display and drivers
+ * @param
+ * @param
+ * @param
+ * @return void
+ * @author Matteo Gottardelli (Primary author)
+ * @author Niccolo Cristoforetti (Helper)
+ * @author Matteo Gottardelli (Maintainer)
+ * @date 2024-10-5
+ */
 void handleIn(Graphics_Button array[], uint8_t position, int8_t size) {
     if (array[position].state!=SELECTED) {
         array[position].state=FOCUSED;
@@ -148,6 +267,14 @@ void handleIn(Graphics_Button array[], uint8_t position, int8_t size) {
         drawButton(array[position], SELECTED_COLOR, STANDARD_COLOR, findSelected(array, size));
     }
 }
+/**
+ * @brief initializes the display and drivers
+ * @return void
+ * @author Matteo Gottardelli (Primary author)
+ * @author Niccolo Cristoforetti (Helper)
+ * @author Matteo Gottardelli (Maintainer)
+ * @date 2024-10-5
+ */
 void hardware_Init() {
     graphics_Init();
     configurePortsInput();
@@ -155,7 +282,15 @@ void hardware_Init() {
     before_ADC();
     _adcInit();
 }
-
+/**
+ * @brief initializes the display and drivers
+ * @param
+ * @return void
+ * @author Matteo Gottardelli (Primary author)
+ * @author Niccolo Cristoforetti (Helper)
+ * @author Matteo Gottardelli (Maintainer)
+ * @date 2024-10-5
+ */
 void handle_buttons(Graphics_Button array[]){
     if(position<(sizes[display_position]-2)){
         switch(display_position) {
@@ -183,6 +318,15 @@ void handle_buttons(Graphics_Button array[]){
        }
     }
 }
+/**
+ * @brief initializes the display and drivers
+ * @param
+ * @return void
+ * @author Matteo Gottardelli (Primary author)
+ * @author Niccolo Cristoforetti (Helper)
+ * @author Matteo Gottardelli (Maintainer)
+ * @date 2024-10-5
+ */
 void handleSelection(Graphics_Button array[]) {
     int i;
     for (i=0; i<sizes[display_position]; i++) {
@@ -192,6 +336,15 @@ void handleSelection(Graphics_Button array[]) {
     position=sizes[display_position]-1;
     array[position].state=FOCUSED;
 }
+/**
+ * @brief initializes the display and drivers
+ * @param
+ * @return void
+ * @author Matteo Gottardelli (Primary author)
+ * @author Niccolo Cristoforetti (Helper)
+ * @author Matteo Gottardelli (Maintainer)
+ * @date 2024-10-5
+ */
 void handleDeselection(Graphics_Button array[]) {
     int i;
     for (i=0; i<sizes[display_position]; i++) {
@@ -200,6 +353,16 @@ void handleDeselection(Graphics_Button array[]) {
     position=sizes[display_position]-1;
     array[position].state=DISABLED;
 }
+/**
+ * @brief initializes the display and drivers
+ * @param
+ * @param
+ * @return void
+ * @author Matteo Gottardelli (Primary author)
+ * @author Niccolo Cristoforetti (Helper)
+ * @author Matteo Gottardelli (Maintainer)
+ * @date 2024-10-5
+ */
 void initArray(Graphics_Button array[], int size) {
     if (position!=size-2) {
         array[size-2]=prevButton;
@@ -214,6 +377,14 @@ void initArray(Graphics_Button array[], int size) {
         array[position].state=FOCUSED;
     }
 }
+/**
+ * @brief initializes the display and drivers
+ * @return void
+ * @author Matteo Gottardelli (Primary author)
+ * @author Niccolo Cristoforetti (Helper)
+ * @author Matteo Gottardelli (Maintainer)
+ * @date 2024-10-5
+ */
 void acquireMutex() {
     __disable_irq();
     if(!mutex) {
@@ -222,6 +393,14 @@ void acquireMutex() {
     }
     __enable_irq();
 }
+/**
+ * @brief initializes the display and drivers
+ * @return void
+ * @author Matteo Gottardelli (Primary author)
+ * @author Niccolo Cristoforetti (Helper)
+ * @author Matteo Gottardelli (Maintainer)
+ * @date 2024-10-5
+ */
 void releaseMutex() {
     __disable_irq();
     mutex=false;
