ec103887985fa9655dc82f0046f132b7bc527d7e
niccolocristoforetti <113518101+niccolocristoforetti@users.noreply.github.com>
2025-02-10 20:33:16 +0100

    Update gamelogic3.h

--- general/gamelogic3.h
+++ general/gamelogic3.h
@@ -17,24 +17,175 @@
 #include "gamelogic1.h"
 
 //Function declarations
-void fn_ELABORATE_TENT(void);  //Function for ELABORATE_TENT state
-void fn_INCREMENT_TENT(void);  //Function for INCREMENT_TENT state
-void fn_GAME_OVER(void);  //Function for GAME_OVER state
-void fn_EASY_MODE(void);  //Function for EASY_MODE state
-void fn_MEDIUM_MODE(void); //Function for MEDIUM_MODE state
-void fn_DIFFICULT_MODE(void);  //Function for DIFFICULT_MODE state
-void fn_ELABORATE_RESULT(void); //Function for ELABORATE_RESULT state
-void fn_WIN(void);  //Function for WIN state
-int16_t checkIfTenctOrCorrection(int8_t c); //Function that checks if the sequence is a tenctative or a correction. Returns a int for set the right index in the chronology
-void insertInChronologyRes(int8_t* seq); //Function that inserts sequence or sequence correction in chronology
-void insertInChronologyColor(int8_t* seq); //Function that inserts sequence or sequence correction in chronology
-void incrementTent(void); //Function that increases the number of tenctative
-bool checkIfGameOver(void);  //Function that checks if the maximum number of attempts has been reached
-int8_t check_difficulty(void);  //Function that checks the difficulty degree
-void correctionsEasyMode(void); //Function that transforms the seq_user into corrections in EASY_MODE
-void correctionsMediumMode(void); //Function that transforms the seq_user into corrections in MEDIUM_MODE
-void correctionsDifficultMode(void); //Function that transforms the seq_user into corrections in DIFFICULT_MODE
-void printLastTentativeInChronology(void);  //Function that prints the last sequence the last sequence inserted in the chronology with the respective corrections
-bool winCondition(void); //Function that checks if the next state is WIN or not
 
+/**
+ * @brief Function for ELABORATE_TENT state
+ * @param void
+ * @return void
+ * @author Niccolò Cristoforetti (Primary Author & Maintainer)
+ * @date 2024-09-29
+ */
+void fn_ELABORATE_TENT(void);
+
+/**
+ * @brief Function for INCREMENT_TENT state
+ * @param void
+ * @return void
+ * @author Niccolò Cristoforetti (Primary Author & Maintainer)
+ * @date 2024-09-29
+ */
+void fn_INCREMENT_TENT(void);
+
+/**
+ * @brief Function for GAME_OVER state
+ * @param void
+ * @return void
+ * @author Niccolò Cristoforetti (Primary Author & Maintainer)
+ * @date 2024-09-29
+ */
+void fn_GAME_OVER(void);
+
+/**
+ * @brief Function for EASY_MODE state
+ * @param void
+ * @return void
+ * @author Niccolò Cristoforetti (Primary Author & Maintainer)
+ * @date 2024-09-29
+ */
+void fn_EASY_MODE(void);
+
+/**
+ * @brief Function for MEDIUM_MODE state
+ * @param void
+ * @return void
+ * @author Niccolò Cristoforetti (Primary Author & Maintainer)
+ * @date 2024-09-29
+ */
+void fn_MEDIUM_MODE(void);
+
+/**
+ * @brief Function for DIFFICULT_MODE state
+ * @param void
+ * @return void
+ * @author Niccolò Cristoforetti (Primary Author & Maintainer)
+ * @date 2024-09-29
+ */
+void fn_DIFFICULT_MODE(void);
+
+/**
+ * @brief Function for ELABORATE_RESULT state
+ * @param void
+ * @return void
+ * @author Niccolò Cristoforetti (Primary Author & Maintainer)
+ * @date 2024-09-29
+ */
+void fn_ELABORATE_RESULT(void);
+
+/**
+ * @brief Function for WIN state
+ * @param void
+ * @return void
+ * @author Niccolò Cristoforetti (Primary Author & Maintainer)
+ * @date 2024-09-29
+ */
+void fn_WIN(void);
+
+/**
+ * @brief Function that checks if the sequence is a tentative or a correction.
+ * @param integer
+ * @return integer for set the right index in the chronology
+ * @author Niccolò Cristoforetti (Primary Author & Maintainer)
+ * @date 2024-09-29
+ */
+//int16_t checkIfTenctOrCorrection(int8_t c);
+
+/**
+ * @brief Function that inserts sequence results in chronology
+ * @param array integer for the sequence results
+ * @return void
+ * @author Niccolò Cristoforetti (Primary Author & Maintainer)
+ * @date 2024-09-29
+ */
+void insertInChronologyRes(int8_t* seq);
+
+/**
+ * @brief Function that inserts sequence in chronology
+ * @param array integer for the sequence
+ * @return void
+ * @author Niccolò Cristoforetti (Primary Author & Maintainer)
+ * @date 2024-09-29
+ */
+void insertInChronologyColor(int8_t* seq);
+
+/**
+ * @brief Function that increases the number of tentative
+ * @param void
+ * @return void
+ * @author Niccolò Cristoforetti (Primary Author & Maintainer)
+ * @date 2024-09-29
+ */
+void incrementTent(void);
+
+/**
+ * @brief Function that checks if the maximum number of attempts has been reached
+ * @param void
+ * @return boolean that check the gameover state
+ * @author Niccolò Cristoforetti (Primary Author & Maintainer)
+ * @date 2024-09-29
+ */
+bool checkIfGameOver(void);
+
+/**
+ * @brief Function that checks the difficulty degree
+ * @param void
+ * @return integer that shows the difficulty degree
+ * @author Niccolò Cristoforetti (Primary Author & Maintainer)
+ * @date 2024-09-29
+ */
+int8_t check_difficulty(void);
+
+/**
+ * @brief Function that transforms the seq_user into corrections in EASY_MODE
+ * @param void
+ * @return void
+ * @author Niccolò Cristoforetti (Primary Author & Maintainer)
+ * @date 2024-09-29
+ */
+void correctionsEasyMode(void);
+
+/**
+ * @brief Function that transforms the seq_user into corrections in MEDIUM_MODE
+ * @param void
+ * @return void
+ * @author Niccolò Cristoforetti (Primary Author & Maintainer)
+ * @date 2024-09-29
+ */
+void correctionsMediumMode(void);
+
+/**
+ * @brief Function that transforms the seq_user into corrections in DIFFICULT_MODE
+ * @param void
+ * @return void
+ * @author Niccolò Cristoforetti (Primary Author & Maintainer)
+ * @date 2024-09-29
+ */
+void correctionsDifficultMode(void);
+
+/**
+ * @brief Function that prints the last sequence inserted in the chronology with the respective corrections
+ * @param void
+ * @return void
+ * @author Niccolò Cristoforetti (Primary Author & Maintainer)
+ * @date 2024-09-29
+ */
+void printLastTentativeInChronology(void);
+
+/**
+ * @brief Function that checks if the next state is WIN or not
+ * @param void
+ * @return boolean that check the win state
+ * @author Niccolò Cristoforetti (Primary Author & Maintainer)
+ * @date 2024-09-29
+ */
+bool winCondition(void);
 #endif
