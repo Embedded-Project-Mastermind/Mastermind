94cda4cd6d146aa0de39a17dd4f3b9071e53abf7
ale-bena <127418373+ale-bena@users.noreply.github.com>
2025-02-10 15:51:42 +0100

    Update dimension.h

--- general/dimension.h
+++ general/dimension.h
@@ -12,14 +12,61 @@
 #include "implementations.h"
 #include "graphics.h"
 
-
+/**
+ * @brief draws buttons
+ * @return TODO: Describe return value.
+ * @author Alessandro Benassi (Primary Author & Maintainer)
+ * @date 2024-10-10
+ */
 void drawDimension(); //draws buttons
+/**
+ * @brief general function for scenario drawing
+ * @return void
+ * @author Alessandro Benassi (Primary Author & Maintainer)
+ * @date 2024-10-10
+ */
 void fn_DIMENSION(void); //general function for scenario drawing
+/**
+ * @brief manages the font dimension for the buttons
+ * @return void
+ * @author Alessandro Benassi (Primary Author & Maintainer)
+ * @date 2024-10-10
+ */
 void handleFont(); //manages the font dimension for the buttons
+/**
+ * @brief handles the up movement of the stick
+ * @return void
+ * @author Alessandro Benassi (Primary Author & Maintainer)
+ * @date 2024-10-10
+ */
 void upStick_DIMENSION(); //handles the up movement of the stick
+/**
+ * @brief handles the down movement of the stick
+ * @return void
+ * @author Alessandro Benassi (Primary Author & Maintainer)
+ * @date 2024-10-10
+ */
 void downStick_DIMENSION(); //handles the down movement of the stick
+/**
+ * @brief handles the down movement of the stick
+ * @return void
+ * @author Alessandro Benassi (Primary Author & Maintainer)
+ * @date 2024-10-10
+ */
 void leftStick_DIMENSION(); //handles the left movement of the stick
+/**
+ * @brief handles the right movement of the stick
+ * @return void
+ * @author Alessandro Benassi (Primary Author & Maintainer)
+ * @date 2024-10-10
+ */
 void rightStick_DIMENSION(); //handles the right movement of the stick
+/**
+ * @brief extern declaretion
+ * @return void
+ * @author Alessandro Benassi (Primary Author & Maintainer)
+ * @date 2024-10-10
+ */
 void handlePressure_DIMENSION();
 //extern declaretion
 extern Graphics_Button dim_buttons[];  //array of buttons declaration
