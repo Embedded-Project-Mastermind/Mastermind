45c997518ee26828e69d8303df68367ac42b0338
Gotta003 <94313973+Gotta003@users.noreply.github.com>
2025-02-11 16:27:56 +0100

    Doxygen

--- html/search/variables_5.js
+++ html/search/variables_5.js
@@ -1,6 +1,7 @@
 var searchData=
 [
-  ['flags_0',['flags',['../struct_game.html#af3536e48f60e7f23419a97fea48afbb8',1,'Game']]],
-  ['folder_5fpath_1',['folder_path',['../namespacecomment.html#aa390eaf55023f5f029d147c1b897d054',1,'comment']]],
-  ['fsm_2',['fsm',['../general_2main_8c.html#a3cdbaf41c8d20ef6a9e9a42a7ba77aa7',1,'fsm:&#160;main.c'],['../src_2main_8c.html#a3cdbaf41c8d20ef6a9e9a42a7ba77aa7',1,'fsm:&#160;main.c']]]
+  ['flagsguess_0',['flagsGuess',['../struct_game.html#a52c30108d0d1a24ff70fcd4a8c420b5b',1,'Game']]],
+  ['flagsusr_1',['flagsUsr',['../struct_game.html#aecfbeb2d860d8f03260e7df95cb90f6d',1,'Game']]],
+  ['folder_5fpath_2',['folder_path',['../namespacecomment.html#aa390eaf55023f5f029d147c1b897d054',1,'comment']]],
+  ['fsm_3',['fsm',['../general_2main_8c.html#a3cdbaf41c8d20ef6a9e9a42a7ba77aa7',1,'fsm:&#160;main.c'],['../src_2main_8c.html#a3cdbaf41c8d20ef6a9e9a42a7ba77aa7',1,'fsm:&#160;main.c']]]
 ];
